9a5c6b476156d4b4b48ea25863fcafacee498375
Jowensky Neard <jp.12@live.com>
2018-11-13 12:37:27 -0500

    refernished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,144 @@
-var correct = 0;
-var outOf = 10;
-var time = 30
-var Answers = ['a', 'b', 'c', 'd', 'd', 'b', 'b', 'd', 'b', 'c']
+$(document).ready(function() {
+  game.startGame()
+});
 
-var intervalId;
+var Questions = [
+  {
+    question: "What is my birthday?",
+    answers: [
+      "feb 1",
+      "jan 1",
+      "I've always been here",
+      "birthdays are a made up idea"
+    ],
+    correctAnswer: "feb 1"
+  },
+  {
+    question: "What is Chicago's Capital?",
+    answers: ["What?", "absent", "today", "not applicable"],
+    correctAnswer: "absent"
+  },
+  {
+    question: "What is the best taco?",
+    answers: ["Asado", "Barbacoa", "steak", "chorizo"],
+    correctAnswer: "steak"
+  },
+  {
+    question: "Top 3 psychedelic Bands?",
+    answers: [
+      "Led Zeppelin, The Yardbirds, FUzz",
+      "MGMT, Grateful Dead, King Crimson",
+      "Tame Impala, Jimi Hendrix, Hawkwind",
+      "Fuzz, Led Zeppelin, Electric Wizards"
+    ],
+    correctAnswer: "Fuzz, Led Zeppelin, Electric Wizards"
+  },
+  {
+    question: "What is Code?",
+    answers: ["life", "a headache", "synax", "instructions"],
+    correctAnswer: "instructions"
+  },
+  {
+    question: "Best place to live?",
+    answers: ["A forest", "In a tree", "By a pirate bird", "don't live"],
+    correctAnswer: "In a tree"
+  },
+  {
+    question: "Why am I always tired?",
+    answers: ["Just go to sleep", "I don't know", "Check question 5", "other"],
+    correctAnswer: "I don't know"
+  },
+  {
+    question: "Synax?",
+    answers: ["yes", "yeaa", "no", "yea"],
+    correctAnswer: "yea"
+  },
+  {
+    question: "Top Beverage?",
+    answers: ["Coke before 1903", "Water", "Coke after 1903"],
+    correctAnswer: "Water"
+  },
+  {
+    question: "Did good?",
+    answers: ["yes", "no", "Doubt it", "maybe"],
+    correctAnswer: "Doubt it"
+  }
+];
 
-function grabScore() {
-    var urChoice = [
-        $('input[name=Question1]:checked').val(),
-        $('input[name=Question2]:checked').val(),
-        $('input[name=Question3]:checked').val(),
-        $('input[name=Question4]:checked').val(),
-        $('input[name=Question5]:checked').val(),
-        $('input[name=Question6]:checked').val(),
-        $('input[name=Question7]:checked').val(),
-        $('input[name=Question8]:checked').val(),
-        $('input[name=Question9]:checked').val(),
-        $('input[name=Question10]:checked').val()
-    ]
+var correct = 0
+var wrong = 0
 
-    for (var i = 0; i < 10; i++) {
-        if (urChoice[i] === Answers[i]) {
-            correct++;
-        }
+// make resart game function
+var game = {
+  startGame: function() {
+
+   
+    game.renderQuestion();
+
+    $(".answers").on("click", game.handleSubmit);
+  
+    game.timer();
+  },
+  currentQuestion: 0,
+  timer: function() {
+    var timeLeft = 10;
+
+    $("#timer").html(timeLeft);
+
+    losingTime = setInterval(function() {
+      if (timeLeft > 0) {
+        timeLeft--;
+        $("#timer").html(timeLeft);
+      } else {
+        clearInterval(losingTime)
+        game.nextQuestion()
+      }
+    }, 1 * 1000);
+  },
+  nextQuestion: function() {
+    if (this.currentQuestion < Questions.length -1) {
+      this.currentQuestion++;
+      this.timer();
+      this.renderQuestion();
+    } else {
+      game.setUp()
+      $('#timer').html("")
+      $('.score').html(`You got ${correct} out of ${Questions.length}`)
     }
-    $('#container').empty();
-    $('header').empty();
-    $('#yougot').append(correct+'c')
-    $('#reload').show()
-    clearInterval(intervalId)  
+  },
+  renderQuestion: function() {
+    var newQuestion = Questions[this.currentQuestion];
+    $("#question").html(newQuestion.question);
+    $('#answers').html("")
+
+    for (var i = 0; i < newQuestion.answers.length; i++) {
+      $("#answers").append(`<button class="answers" data-answer="${newQuestion.answers[i]}">${newQuestion.answers[i]}</button><br>`);
+    }
+    this.answers()
+  },
+  handleSubmit: function(event) {
+    correctAnswer = Questions[game.currentQuestion].correctAnswer;
+    selectedAnswer = $(event.target).data("answer");
+
+    if (correctAnswer === selectedAnswer) {
+      game.emptyPage();
+      correct++
+    } else {
+      game.emptyPage();
+      wrong++
+    }
+  },
+  emptyPage: function() {
+   this.setUp()
+    this.nextQuestion();
+  }, 
+  setUp: function () {
+    $("#question").html("");
+    $("#answers").html("");
+    clearInterval(losingTime);
+  },
+  answers: function() {
+
+  $(".answers").on("click", game.handleSubmit);
+  }
 };
-
-$(document).ready(function () {
-
-    intervalId = setInterval(function () {
-        time--
-        $('header').html(time)
-        if (time === 0) {
-            grabScore()
-        }
-    }, 1 * 1000)
-
-    $('input[type=submit]').click(function (e) {
-            e.preventDefault()
-            grabScore()
-        })
-})
-
-$('#reload').on('click', function () {
-    location.reload()
-})
-
-
-
